924804c27d40cf0b3b383aadaebf3fa38c5babb1
Sergio <sergio977@rambler.ru>
2022-11-24 16:34:31 +0500

    add Ex_2

---
+++ Ex_2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[,] matrix = new int[3, 4];
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+PrintArray(matrix);
